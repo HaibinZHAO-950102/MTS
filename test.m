a4831a6276d465b97d155f82b93a6c1108e7b664
Haibin ZHAO <zhb950102@qq.com>
2020-04-12 20:38:23 +0200

    presentation slides and manuscript

--- test.m
+++
@@ -1,3 +0,0 @@
-clc
-[validationresult(:,3),voltage_theory_quantization(:,1),validationresult(:,5),voltage_theory_quantization(:,2),validationresult(:,7),voltage_theory_quantization(:,3),validationresult(:,9),voltage_theory_quantization(:,4);validation_evaluation',NaN(1,10)]
-[]
